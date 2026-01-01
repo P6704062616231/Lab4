ae193a52e0a07974462172c3f1f3a2d207de8659
6704062616231 <s6704062616231@email.kmutnb.ac.th>
2026-01-01 18:28:57 +0700

    grade version 3

---
+++ lab4no10.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+int main(){
+int score,a = 0,b = 0,c = 0,d = 0,f = 0;
+    while (1){
+        scanf("%d",&score);
+        if (score >= 68) {
+            if (score >= 85){
+                printf("%d(A)\n",score);
+                a++;
+            }else if (score >= 75){
+                printf("%d(B)\n",score);
+                b++;
+            }else{
+                printf("%d(C)\n",score);
+                c++;
+            }
+        } else {
+            if (score < 0){
+                if(score > -2){
+                    break;
+                }
+            }
+            else if (score >= 55){
+                printf("%d(D)\n",score);
+                d++;
+            }else{
+                printf("%d(F)\n",score);
+                f++;
+            }
+        }
+    }
+
+    printf("A(%d)\n",a);
+    printf("B(%d)\n",b);
+    printf("C(%d)\n",c);
+    printf("D(%d)\n",d);
+    printf("F(%d)\n",f);
+return 0;
+}
